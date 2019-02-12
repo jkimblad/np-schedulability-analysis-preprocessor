d9701b48e759d6b258f9bcf7411c5802612978a6
jacobki <jacobki@kth.se>
2019-02-12 17:00:59 +0100

    Parse input options

--- preprocessor.py
+++ preprocessor.py
@@ -1,9 +1,12 @@
+#!/usr/bin/env python3
+
 # Import libraries
 # To read CSV input
 import csv
 # For LCM
 from gmpy2 import lcm
 import sys
+import argparse
 
 # Constants defining input
 EXPECTED_COLUMNS = 8
@@ -14,33 +17,50 @@ EXPECTED_ARGUMENTS = 2
 # Main, execution starts here
 def main():
 
-    # Check correct amount of arguments are provided
-    if (len(sys.argv) != EXPECTED_ARGUMENTS):
-        print("Expected exactly " + str(EXPECTED_ARGUMENTS - 1) + " arguments, " + str(len(sys.argv) - 1) + " was provided")
-        sys.exit()
+    # Get user options
+    inputArguments = inputParse()
 
     # Get input tasks as a list of dictionaries
     tasks = getTasks()
 
-    # Calculate hyperperiod of task set
-    hyperPeriod = 1;
-    for task in tasks:
-        hyperPeriod = lcm(hyperPeriod, task['period'])
+    #Generate normal jobs
+    if not(inputArguments.aerModel):
+        # Calculate hyperperiod of task set
+        hyperPeriod = 1;
+        for task in tasks:
+            hyperPeriod = lcm(hyperPeriod, task['period'])
 
-    # Generate each job for each task in the set
-    taskJobs = []
-    for task in tasks:
-        taskJobs.append(generateJobs(task, hyperPeriod))
+        # Generate each job for each task in the set
+        taskJobs = []
+        for task in tasks:
+            taskJobs.append(generateJobs(task, hyperPeriod))
 
-    # Generate the csv output
-    fieldNames = ['Task ID', 'Job ID', 'Arrival min', 'Arrival max', 'Cost min', 'Cost max', 'Deadline', 'Priority']
-    writer = csv.DictWriter(sys.stdout, fieldNames)
-    writer.writeheader()
-    for task in taskJobs:
-        for job in task:
-            writer.writerow(job)
+        # Generate the csv output
+        fieldNames = ['Task ID', 'Job ID', 'Arrival min', 'Arrival max', 'Cost min', 'Cost max', 'Deadline', 'Priority']
+        writer = csv.DictWriter(inputArguments.outputFile, fieldNames)
+        writer.writeheader()
+        for task in taskJobs:
+            for job in task:
+                writer.writerow(job)
+
+    #Generate A and R jobs
+    else:
 
     
+def inputParse():
+    parser = argparse.ArgumentParser()
+
+    # Input file
+    parser.add_argument("inputFile", help="Input FILE in .csv format", metavar="input")
+
+    # Output file
+    parser.add_argument("-o", "--output", dest="outputFile", help="Output FILE", metavar="FILE", default=sys.stdout)
+
+    # Create A and R jobs
+    parser.add_argument("--aer", action="store_true", dest="aerModel", help="Input is transformed to A and R jobs to be scheduled onto memory")
+
+    return parser.parse_args()
+
 
 # Return the jobs that a single task would produce within a given hyperperiod as a list of list
 def generateJobs(task, hyperPeriod):
--- task_generator.py
+++ task_generator.py
@@ -1,6 +1,12 @@
+#!/usr/bin/env python3
+
+# The unit of the program output is micro seconds due to the periods being used
+# are from AUTOSAR. The output can be converted to any suitable time unit.
+
 #Import libraries
 import sys
 import random
+import csv
 # numpy is used to create custom discrete distributions
 import numpy as np
 
@@ -24,46 +30,161 @@ def main():
 
     #Randomise utilizations
     utilizations = uUniFast(taskAmountArg, utilizationArg)
-    print (utilizations)
+    # print (utilizations)
 
     #Randomize periods, assume periods are equal to deadlines
     periods = generatePeriods(taskAmountArg)
-    print(periods)
+    # print(periods)
 
     #Calculate computation times
     computationTimes = calculateComputationTimes(periods, utilizations)
-    print (computationTimes)
+    # print (computationTimes['bcet'])
+    # print (computationTimes['wcet'])
 
     #Randomize release times
     releaseTimes = generateReleaseTimes(taskAmountArg)
 
     #Randomize priorities
     priorities = generatePriorities(taskAmountArg)
+    # print(priorities)
+
+    printTasks(utilizations, periods, computationTimes, releaseTimes, priorities)
+    
+def printTasks(utilizations, periods, computationTimes, releaseTimes, priorities):
+
+    fieldNames = ["id", "arrival_min", "arrival_max", "computation_min", "computation_max", "deadline", "priority", "period"]
+    writer = csv.writer(sys.stdout, fieldNames)
+    writer.writerow(fieldNames)
+
+    for i in range(len(utilizations)):
+        task = []
+
+        #id
+        task.append(i + 1)
+
+        #release
+        task.append(releaseTimes['bcrt'][i])
+        task.append(releaseTimes['wcrt'][i])
+
+        #computation
+        #min
+        task.append(computationTimes['bcet'][i])
+        #max
+        task.append(computationTimes['wcet'][i])
+
+        #deadline
+        #deadline equals period
+        task.append(periods[i])
+
+        #priority
+        #prio is currently rms
+        task.append(periods[i])
+
+        #period
+        task.append(periods[i])
+
+        writer.writerow(task)
+
+
+def generateAcquisitionTimes(taskAmount):
+    # Acquisition times is represented as nS
+    # These times are acquired from real world examples presented in
+    # "Scheduling Multi-Rate Real-Time Applications on Clustered
+    # Many-Core Architectures with Memory Constraints"
+    codeAcquisitionTimes = [11970, 15543, 12070, 13188, 14858, 12140, 15413, 27593, 15413, 27593, 27730, 15058, 14948, 30470, 14948, 30470, 14945, 14858, 12130, 15300]
+    labelAcquisitionTimes = [908, 958, 908, 908, 55, 990, 1013, 990, 1013, 1148, 1060, 988, 965, 933, 908, 28, 985]
+
+    acquisitionTimes = []
+
+    for i in range(taskAmount):
+        codeAcquisitionTime = codeAcquisitionTimes[random.randint(0, len(codeAcquisitionTimes) - 1)]
+        labelAcquisitionTime = labelAcquisitionTimes[random.randint(0, len(labelAcquisitionTimes) - 1)]
+        acquisitionTimes.append(codeAcquisitionTime + labelAcquisitionTime)
+
+    return acquisitionTimes
+
+
+def generateRestitutionTumes(taskAmount):
+    # Restitution times is represented as nS
+    # These times are acquired from real world examples presented in
+    # "Scheduling Multi-Rate Real-Time Applications on Clustered
+    # Many-Core Architectures with Memory Constraints"
+    restitutionTimes = [25, 195, 28, 55, 55, 28, 55, 83, 55, 83, 28, 25, 28, 390, 240, 0, 28, 195]
+
+    restitutions = []
+
+    for i in range(taskAmount):
+        restitutions.append(restitutionTimes[random.randint(0, len(restitutionTimes) - 1)])
+
+    return restitutions
 
 
 def generatePriorities(taskAmount):
-    None
+    priorities = list(range(1, taskAmount + 1))
+    random.shuffle(priorities)
+    return priorities
 
 
 def generateReleaseTimes(taskAmount):
-    None
+    # Best Case Release-Time
+    bcrt = []
+    # Worst Case Release-Time
+    wcrt = []
+    for i in range (taskAmount):
+        bcrt.append(random.randint(0, 1))
+        wcrt.append(random.randint(1, 2))
+
+    releaseTimes = {'bcrt' : bcrt, 'wcrt' : wcrt}
+    return releaseTimes
 
 
 def calculateComputationTimes(periods, utilizations):
-    computationTimes = []
-    for i in range(0, len(periods)):
-        computationTimes.append(int(periods[i] * utilizations[i]))
+    bcet = []
+    wcet = []
 
+    # Used to translate a period into an index for the bcet and wcet lists
+    periodsEnum = {'100' : 0, '200' : 1, '500' : 2, '1000' : 3, '2000' : 4, '5000' : 5, '10000' : 6, '20000' : 7, '100000' : 8}
+
+    # The following bcet and wcet factors are given by the free real benchmarks for the automotive industry
+    # Best-case execution time as intervals of factors of the average case
+    bcetFactors = [[0.19, 0.92], [0.12, 0.89], [0.17,  0.94], [0.05, 0.99], [0.11, 0.98], [0.32, 0.95], [0.09, 0.99], [0.45,0.98], [0.68, 0.80]]
+    # Worst-case execution time as intervals of factors of the average case
+    wcetFactors = [[1.30, 29.11], [1.54, 19.04], [1.13, 18.44], [1.06, 30.03], [1.06, 15.61], [1.13, 7.76], [1.02, 8.88], [1.03, 4.90], [1.84, 4.75]]
+
+    # Iterate through each period
+    for i in range(0, len(periods)):
+        # TODO Randomize bcet from interval using some probability distribution
+        # The distribution for bcet should however be flipped (more higher
+        # values that are closer to the acet)
+
+        bcetMin = bcetFactors[periodsEnum[str(periods[i])]][0] 
+        bcetMax = bcetFactors[periodsEnum[str(periods[i])]][1] 
+        bcetRange = bcetMax - bcetMin
+
+        # For now, use the optimal BCET within the given interval
+        bcetFactor = bcetMax
+        bcet.append(int(periods[i] * utilizations[i] * bcetFactor))
+
+        # We do the same for wcet
+        wcetMin = wcetFactors[periodsEnum[str(periods[i])]][0] 
+        wcetMax = wcetFactors[periodsEnum[str(periods[i])]][1] 
+        wcetRange = wcetMax - wcetMin
+
+        wcetFactor = wcetMin
+        wcet.append(int(periods[i] * utilizations[i] * wcetFactor))
+
+    # Return results as a dictionary containing the two lists
+    computationTimes = { 'bcet' : bcet, 'wcet' : wcet }
     return computationTimes
         
 
 
-# Generate periods of tasks based on 
+# Generate periods of tasks based on real benchmarks
 def generatePeriods(taskAmount):
     # These are taken from "Real World Automotive Benchmarks For Free" and
     # adapted (with roundings) due to the fact that we exclude angle-synchronous
     # periods 
-    # Periods measured in ns
+    # Periods measured in micro seconds
     periods = [100, 200, 500, 1000, 2000, 5000, 10000, 20000, 100000]
     # periods = [1, 2, 5, 10, 20, 50, 100, 200, 1000]
     periodDistribution = [0.04, 0.02, 0.02, 0.29, 0.29, 0.04, 0.24, 0.01, 0.05]
@@ -86,9 +207,7 @@ def uUniFast(taskAmount, utilization):
     return utilizations
 
 
-
-
-
 if __name__ == '__main__':
     main()
+
 
