0605d170c5ee8dba7065a99374d0c172ca8b02ff
jacobki <jacobki@kth.se>
2019-03-19 16:51:27 +0100

    Iterate over w-ratios

--- run_experiments.py
+++ run_experiments.py
@@ -24,7 +24,9 @@ settings = {
         # At what window ratio should we start exploring
         # starting_window_ratio : None
         # ending_window_ratio : None
-        'window_ratio' : 0.5,
+        'window_ratio_start' : 0.1,
+        'window_ratio_end' : 0.9,
+        'window_ratio_step_size' : 0.1,
 
         # How much should we increment the window ratio
         # window_ratio_step_size :  None
@@ -47,77 +49,81 @@ iteration_counter = 0
 
 # Start experiments
 utilization = settings['starting_utilization']
+window_ratio = settings['window_ratio_start']
 
 # Set random seed
 random.seed(settings['seed'])
 
+while window_ratio <= settings['window_ratio_end']: 
 
-while utilization < settings['ending_utilization']:
+    while utilization <= settings['ending_utilization']:
 
-    for i in range(settings['iterations']):
+        for i in range(settings['iterations']):
 
-        # Generate task-set
-        task_output = subprocess.check_output([                                     \
-                "./task_generator.py",                                              \
-                "-o",                                                               \
-                "tasks/task_set_" + str(iteration_counter) + ".csv",                \
-                "-s",                                                               \
-                str(random.randint(1, 1000000)),                                    \
-                "-a",                                                               \
-                str(settings['a_ratio']),                                                       \
-                str(settings['r_ratio']),                                                       \
-                str(settings['task_amount']),                                                   \
-                str(utilization)                                                    \
-                ])
-        # print("task: " + task_output.decode())
+            # Generate task-set
+            task_output = subprocess.check_output([                                     \
+                    "./task_generator.py",                                              \
+                    "-o",                                                               \
+                    "tasks/task_set_" + str(iteration_counter) + ".csv",                \
+                    "-s",                                                               \
+                    str(random.randint(1, 1000000)),                                    \
+                    "-a",                                                               \
+                    str(settings['a_ratio']),                                                       \
+                    str(settings['r_ratio']),                                                       \
+                    str(settings['task_amount']),                                                   \
+                    str(utilization)                                                    \
+                    ])
+            # print("task: " + task_output.decode())
 
-        # Generate job-set from task-set
-        job_output = subprocess.check_output([                                      \
-                "./preprocessor.py",                                                \
-                "-o",                                                               \
-                "jobs/job_set_" + str(iteration_counter) + ".csv",                  \
-                "-w",                                                               \
-                str(settings['window_ratio']),                                                  \
-                "tasks/task_set_" + str(iteration_counter) + ".csv"                 \
-                ])
-        # print("job: " + job_output.decode())
+            # Generate job-set from task-set
+            job_output = subprocess.check_output([                                      \
+                    "./preprocessor.py",                                                \
+                    "-o",                                                               \
+                    "jobs/job_set_" + str(iteration_counter) + ".csv",                  \
+                    "-w",                                                               \
+                    str(window_ratio),                                                  \
+                    "tasks/task_set_" + str(iteration_counter) + ".csv"                 \
+                    ])
+            # print("job: " + job_output.decode())
 
-        # Run nptest
-        np_result = subprocess.check_output([                                       \
-                "./nptest",                                                         \
-                "-l",                                                               \
-                str(settings['timeout']),                                                       \
-                "-i",                                                               \
-                "AER",                                                              \
-                "-m",                                                               \
-                str(settings['core_amount']),                                                   \
-                "jobs/job_set_" + str(iteration_counter) + ".csv"                   \
-                ])
-        # print("np" + np_result.decode())
+            # Run nptest
+            np_result = subprocess.check_output([                                       \
+                    "./nptest",                                                         \
+                    "-l",                                                               \
+                    str(settings['timeout']),                                                       \
+                    "-i",                                                               \
+                    "AER",                                                              \
+                    "-m",                                                               \
+                    str(settings['core_amount']),                                                   \
+                    "jobs/job_set_" + str(iteration_counter) + ".csv"                   \
+                    ])
+            # print("np" + np_result.decode())
 
-        # Save results
-        temp = {
-            'utilization' : utilization,
-            'success' : int(np_result.decode().replace(" ", "").split(",")[1])
+            # Save results
+            temp = {
+                'utilization' : utilization,
+                'success' : int(np_result.decode().replace(" ", "").split(",")[1])
+                }
+            results.append(temp)
+
+
+            iteration_counter += 1
+
+            # Delete created files
+            subprocess.run(["rm -rf tasks/* jobs/*"], shell=True)
+
+        # Increase utilization for next loop
+        utilization += settings['utilization_step_size']
+
+    output = {
+            'settings': settings,
+            'results': results
             }
-        results.append(temp)
 
+    with open("resuts/window_ratio2/window_ratio_" + round(window_ratio * 100), "w+") as f:
+        f.write(json.dumps(output, indent=4))
 
-        iteration_counter += 1
-
-        # Delete created files
-        subprocess.run(["rm -rf tasks/* jobs/*"], shell=True)
-
-    # Increase utilization for next loop
-    utilization += settings['utilization_step_size']
-
-output = {
-        'settings': settings,
-        'results': results
-        }
-
-print(json.dumps(output, indent=4))
-
+    window_ratio += settings['window_ratio_step_size']
 
 
 
