663337f0239a3b7c5db8658d653a897e1e25725a
jkimblad <jacobki@kth.se>
2019-06-14 19:37:26 +0200

    Add axes labels

--- misc/graph_results.py
+++ misc/graph_results.py
@@ -24,18 +24,25 @@ def main():
         # })
     # core_plot = plt.plot('x', 'y', data = frame)
 
+    f = plt.figure()
 
-    plot10 = plt.plot('x', 'y', data = graph_w_ratio('results/w_ratio2/w_ratio_10.json'))
-    plot20 = plt.plot('x', 'y', data = graph_w_ratio('results/w_ratio2/w_ratio_20.json'))
-    plot30 = plt.plot('x', 'y', data = graph_w_ratio('results/w_ratio2/w_ratio_30.json'))
-    plot40 = plt.plot('x', 'y', data = graph_w_ratio('results/w_ratio2/w_ratio_40.json'))
-    plot50 = plt.plot('x', 'y', data = graph_w_ratio('results/w_ratio2/w_ratio_50.json'))
-    plot60 = plt.plot('x', 'y', data = graph_w_ratio('results/w_ratio2/w_ratio_60.json'))
-    plot70 = plt.plot('x', 'y', data = graph_w_ratio('results/w_ratio2/w_ratio_70.json'))
-    plot80 = plt.plot('x', 'y', data = graph_w_ratio('results/w_ratio2/w_ratio_80.json'))
-    plot90 = plt.plot('x', 'y', data = graph_w_ratio('results/w_ratio2/w_ratio_90.json'))
+
+    plot10 = plt.plot('x', 'y', data = graph_w_ratio('results/w_ratio3/w_ratio_10.json'))
+    plot20 = plt.plot('x', 'y', data = graph_w_ratio('results/w_ratio3/w_ratio_20.json'))
+    plot30 = plt.plot('x', 'y', data = graph_w_ratio('results/w_ratio3/w_ratio_30.json'))
+    plot40 = plt.plot('x', 'y', data = graph_w_ratio('results/w_ratio3/w_ratio_40.json'))
+    plot50 = plt.plot('x', 'y', data = graph_w_ratio('results/w_ratio3/w_ratio_50.json'))
+    plot60 = plt.plot('x', 'y', data = graph_w_ratio('results/w_ratio3/w_ratio_60.json'))
+    plot70 = plt.plot('x', 'y', data = graph_w_ratio('results/w_ratio3/w_ratio_70.json'))
+    plot80 = plt.plot('x', 'y', data = graph_w_ratio('results/w_ratio3/w_ratio_80.json'))
+    plot90 = plt.plot('x', 'y', data = graph_w_ratio('results/w_ratio3/w_ratio_90.json'))
     plt.legend((plot10[0], plot20[0], plot30[0], plot40[0], plot50[0], plot60[0], plot70[0], plot80[0], plot90[0]), ('10', '20', '30', '40', '50', '60', '70', '80', '90'))
     plt.show()
+
+    plt.xlabel("Utilization")
+    plt.ylabel("Average schedulability ratio")
+
+    f.savefig("w_ratio3.pdf", bbox_inches='tight')
 
 
 def graph_w_ratio(file_name):
@@ -54,7 +61,6 @@ def graph_w_ratio(file_name):
         x_data.append(utilization)
         utilization += settings['utilization_step_size']
 
-    
     # Create y-axis
     success_list = []
     for result in results:
